13ce1adc62170e1322b533b67b071982de736f80
MaraOD555 <ruffbc@hotmail.com>
2022-05-25 23:18:38 +0300

    Задание 1 Объявление массивов.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1 Объявление 3-х массивов
+        int [] numbers;
+        numbers = new int [3];
+        numbers [0] = 1;
+        numbers [1] = 2;
+        numbers [2] = 3;
+
+        double [] numbers1 = {1.57, 7.654, 9.986};
+
+        int [] jobYears = {2017, 2018, 2019, 2020, 2021};
     }
 }
